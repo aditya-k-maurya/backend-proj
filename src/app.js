6aeb8e190785659d289a62e2724fee91a525e84e
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-05-15 22:23:28 +0530

    debug

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 
 
 //routes import
-import userRouter from './routes/user.routes'
+import userRouter from './routes/user.routes.js'
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
