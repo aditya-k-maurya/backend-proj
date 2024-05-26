b33b4263b7a9b651d5fb75ca9354de651e897787
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-05-26 20:43:22 +0530

    debug

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -42,11 +42,11 @@ router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
 	.route("/avatar")
-	.patch(verifyJWT, upload.single("/avatar"), updateUserAvatar);
+	.patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 router
 	.route("/cover-image")
-	.patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+	.patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
